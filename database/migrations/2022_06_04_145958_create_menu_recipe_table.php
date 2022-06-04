44a596ab3f8b43ff256953a50315160c945cb3fb
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-06-05 00:08:55 +0900

    add migration

---
+++ database/migrations/2022_06_04_145958_create_menu_recipe_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_recipe', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('recipe_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_recipe');
+    }
+};
