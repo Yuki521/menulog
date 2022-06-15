ea8abf5f16754d2c11f17744a960bcbd021efd0e
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-06-15 10:23:28 +0900

    fix design

--- resources/views/Recipe/index.blade.php
+++ resources/views/Recipe/index.blade.php
@@ -1,5 +1,15 @@
-
-@foreach($recipes as $recipe)
-    {{$recipe->title}}
-    {{$recipe->calorie}}
-@endforeach
+<form>
+    <table>
+        <tr>
+            <th>レシピ名</th>
+            <th>カロリー</th>
+            <th>操作</th>
+        </tr>
+        @foreach($recipes as $recipe)
+            <tr>
+                <td>{{$recipe->title}}</td>
+                <td>{{$recipe->calorie}}</td>
+            </tr>
+        @endforeach
+    </table>
+</form>
